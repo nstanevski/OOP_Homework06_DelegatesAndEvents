59eb149ed1fb9b65a28b81ee715fc44e253728e9
nstanevski <nikolay.stanevski@gmail.com>
2015-12-11 23:18:36 +0200

    Task #4 finished

---
+++ 04_StudentClass/PropertyChangedEventArgs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _04_StudentClass
+{
+    public class PropertyChangedEventArgs:EventArgs
+    {
+        public PropertyChangedEventArgs(string propertyName, string oldValue, string newValue)
+        {
+            this.PropertyName = propertyName;
+            this.OldValue = oldValue;
+            this.NewValue = newValue;
+            
+        }
+
+        public string PropertyName { get; set; }
+
+        public string OldValue { get; set; }
+
+        public string NewValue { get; set; }
+    }
+}
---
+++ 04_StudentClass/Student.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace _04_StudentClass
+{
+
+    public delegate void OnPropertyChangedEventHandler(Student sender, PropertyChangedEventArgs args);
+
+    public class Student
+    {
+        private string name;
+        private int age;
+
+        public event OnPropertyChangedEventHandler PropertyChanged;
+        public Student(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("name", "Name cannot be null or empty");
+                }
+                if (this.name != value)
+                {
+                    if (this.PropertyChanged != null)
+                    {
+                        this.PropertyChanged(this, 
+                            new PropertyChangedEventArgs("Name", this.name, value));
+                    }
+                }
+                this.name = value;
+            }
+        }
+
+        public int Age
+        {
+            get { return this.age; }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentOutOfRangeException("age", "Age cannot be null or empty");
+                }
+                if (this.age != value)
+                {
+                    if (this.PropertyChanged != null)
+                    {
+                        this.PropertyChanged(this,
+                            new PropertyChangedEventArgs("Age", this.age.ToString(), value.ToString()));
+                    }
+                }
+                this.age = value;
+            }
+        }
+    }
+}
